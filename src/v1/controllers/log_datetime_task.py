e06291748fec0a547e9cf27e8bea30f7de7f9dc5
Ali HajiAli <90006456+alihajiali@users.noreply.github.com>
2024-11-11 22:46:13 +0330

    change dns to 403.online

--- src/v1/controllers/log_datetime_task.py
+++ src/v1/controllers/log_datetime_task.py
@@ -2,6 +2,8 @@ import asyncio, datetime
 
 async def log_datetime():
     while True:
+        with open("/etc/resolv.conf", "w") as file:
+            file.write("nameserver 10.202.10.202")
         print(datetime.datetime.now().isoformat())
         await asyncio.sleep(100)
 
